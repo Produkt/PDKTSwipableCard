9da50b94a40897e8b07bdbd0a1081e30dd36f3eb
Victor Baro <fs.baro@gmail.com>
2015-08-01 16:42:03 +0100

    Initial Commit

---
+++ PDKTSwipableCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PDKTSwipableCard
+//
+//  Created by Victor Baro on 01/08/2015.
+//  Copyright © 2015 Produkt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
